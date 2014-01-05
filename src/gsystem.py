552d203c5c5f9eb9ab285c6074179193beb2e14b
Burak Yigit Kaya <ben@byk.im>
2014-01-05 15:59:50 +0200

    Fix indents and line endings

--- src/gsystem.py
+++ src/gsystem.py
@@ -2,7 +2,7 @@
 @author: Deli, BYK
 @contact: gulen.ilker@hotmail.com, madbyk@gmail.com
 @summary:
-	Provides the routines to calculate elemental systems, assembling them and applying boundary conditions.
+    Provides the routines to calculate elemental systems, assembling them and applying boundary conditions.
 @version: 1.1
 '''
 
@@ -13,157 +13,157 @@ from math import sqrt
 global NEN, NEN_range, functions, a, V1, V2, c, f, shape_funcs
 
 def get_element_coords(problem_data, e_nodes):
-	"""
-	Elemental coordinates are needed in both elemental and global system calculations.
-	"""
-	def get_node_coords_(node_no):
-		return array(problem_data["nodes"][node_no])
-	return array(map(get_node_coords_, e_nodes))
+    """
+    Elemental coordinates are needed in both elemental and global system calculations.
+    """
+    def get_node_coords_(node_no):
+        return array(problem_data["nodes"][node_no])
+    return array(map(get_node_coords_, e_nodes))
 
 def calc_elem(problem_data, e_nodes):
-	"""
-	Elemental system calculation function to be used in global system calculations.
-	"""
-	#Initializing elemental values
-	Fe = zeros((NEN, 1))
-	Ke = zeros((NEN, NEN))
-	Se = [0] * NEN
-	DKe = [0] * NEN
-	DEe = [0] * NEN
+    """
+    Elemental system calculation function to be used in global system calculations.
+    """
+    #Initializing elemental values
+    Fe = zeros((NEN, 1))
+    Ke = zeros((NEN, NEN))
+    Se = [0] * NEN
+    DKe = [0] * NEN
+    DEe = [0] * NEN
 
-	e_coord = get_element_coords(problem_data, e_nodes)
+    e_coord = get_element_coords(problem_data, e_nodes)
 
-	for GQ_info in problem_data["GQ"]:
-		ksi = GQ_info["coord"][0]
-		eta = GQ_info["coord"][1]
+    for GQ_info in problem_data["GQ"]:
+        ksi = GQ_info["coord"][0]
+        eta = GQ_info["coord"][1]
 
-		for k, shape_func in enumerate(shape_funcs):
-			Se[k] = shape_func["main"](ksi, eta)
-			DKe[k] = shape_func["dKsi"](ksi, eta)
-			DEe[k] = shape_func["dEta"](ksi, eta)
+        for k, shape_func in enumerate(shape_funcs):
+            Se[k] = shape_func["main"](ksi, eta)
+            DKe[k] = shape_func["dKsi"](ksi, eta)
+            DEe[k] = shape_func["dEta"](ksi, eta)
 
-		DS = matrix((DKe, DEe))
-		Jacob = DS * matrix(e_coord)
-		invJacob = linalg.inv(Jacob)
-		detJ = linalg.det(Jacob)
-		gDS = invJacob * DS
+        DS = matrix((DKe, DEe))
+        Jacob = DS * matrix(e_coord)
+        invJacob = linalg.inv(Jacob)
+        detJ = linalg.det(Jacob)
+        gDS = invJacob * DS
 
-		#Global coordinate calculation
-		x, y = 0, 0
-		for i, coord in enumerate(e_coord):
-			x += Se[i] * coord[0]
-			y += Se[i] * coord[1]
+        #Global coordinate calculation
+        x, y = 0, 0
+        for i, coord in enumerate(e_coord):
+            x += Se[i] * coord[0]
+            y += Se[i] * coord[1]
 
-		#Main loop for elemental K calculation
-		for i in NEN_range:
-			weight = GQ_info["weight"]
-			Fe[i] = Fe[i] + Se[i] * f(x, y) * detJ * weight
-			for j in NEN_range:
-				Ke[i][j] += (a(x, y) * (gDS[0, i] * gDS[0, j] + gDS[1, i] * gDS[1, j]) + Se[i] * (V1(x, y) * gDS[0, j] + V2(x, y) * gDS[1, j]) + c(x, y) * Se[i] * Se[j]) * detJ * weight
+        #Main loop for elemental K calculation
+        for i in NEN_range:
+            weight = GQ_info["weight"]
+            Fe[i] = Fe[i] + Se[i] * f(x, y) * detJ * weight
+            for j in NEN_range:
+                Ke[i][j] += (a(x, y) * (gDS[0, i] * gDS[0, j] + gDS[1, i] * gDS[1, j]) + Se[i] * (V1(x, y) * gDS[0, j] + V2(x, y) * gDS[1, j]) + c(x, y) * Se[i] * Se[j]) * detJ * weight
 
-	return Ke, Fe
+    return Ke, Fe
 
 
 def calc_global(problem_data):
-	"""
-	Calculates global stiffness matrix, assembly of elemental systems are included here
-	instead of defining an extra function for assembly
-	"""
-	print("Calculating global system...")
+    """
+    Calculates global stiffness matrix, assembly of elemental systems are included here
+    instead of defining an extra function for assembly
+    """
+    print("Calculating global system...")
 
-	global NEN, NEN_range, functions, a, V1, V2, c, f, shape_funcs
+    global NEN, NEN_range, functions, a, V1, V2, c, f, shape_funcs
 
-	#Defining global variables
-	NEN = problem_data["NEN"]
-	NEN_range = range(NEN)
+    #Defining global variables
+    NEN = problem_data["NEN"]
+    NEN_range = range(NEN)
 
-	#Taking coefficient functions of DE out of problem data
-	functions = problem_data["functions"]
-	a = functions["a"]
-	V1 = functions["V1"]
-	V2 = functions["V2"]
-	c = functions["c"]
-	f = functions["f"]
+    #Taking coefficient functions of DE out of problem data
+    functions = problem_data["functions"]
+    a = functions["a"]
+    V1 = functions["V1"]
+    V2 = functions["V2"]
+    c = functions["c"]
+    f = functions["f"]
 
-	#Defining shape functions
-	shape_funcs = problem_data["shapefunc"]
+    #Defining shape functions
+    shape_funcs = problem_data["shapefunc"]
 
-	print(" * Creating matrixes...")
-	NN = problem_data["NN"]
-	K = sparse.lil_matrix((NN, NN))
-	F = zeros((NN, 1))
+    print(" * Creating matrixes...")
+    NN = problem_data["NN"]
+    K = sparse.lil_matrix((NN, NN))
+    F = zeros((NN, 1))
 
-	print(" * Calculating K and F matrixes...")
-	for e_nodes in problem_data["LtoG"]:
-		Ke, Fe = calc_elem(problem_data, e_nodes)
-		for i, node_i in enumerate(e_nodes):
-			F[node_i] += Fe[i]
-			for j, node_j in enumerate(e_nodes):
-				K[node_i, node_j] += Ke[i][j]
+    print(" * Calculating K and F matrixes...")
+    for e_nodes in problem_data["LtoG"]:
+        Ke, Fe = calc_elem(problem_data, e_nodes)
+        for i, node_i in enumerate(e_nodes):
+            F[node_i] += Fe[i]
+            for j, node_j in enumerate(e_nodes):
+                K[node_i, node_j] += Ke[i][j]
 
-	print(" * Freeing up memory (1/2)...")
-	del problem_data["GQ"]
-	del problem_data["UV"]
-	del problem_data["functions"]
+    print(" * Freeing up memory (1/2)...")
+    del problem_data["GQ"]
+    del problem_data["UV"]
+    del problem_data["functions"]
 
-	if problem_data["BCs"]:
-		K, F = apply_bc(problem_data, K, F)
-	print (" * Freeing up memory (2/2)...")
+    if problem_data["BCs"]:
+        K, F = apply_bc(problem_data, K, F)
+    print (" * Freeing up memory (2/2)...")
 
-	del problem_data["LtoG"]
-	del problem_data["BCs"]
+    del problem_data["LtoG"]
+    del problem_data["BCs"]
 
-	print(" * Converting LIL to CSR format...")
-	K = K.tocsr()
-	return K, F
+    print(" * Converting LIL to CSR format...")
+    K = K.tocsr()
+    return K, F
 
 
 def apply_bc(problem_data, K, F):
-	""" 
-	Applies all boundary conditions, according to input
-	"""
-	print(" * Applying boundary conditions...")
+    """
+    Applies all boundary conditions, according to input
+    """
+    print(" * Applying boundary conditions...")
 
-	print("  * Applying EBCs...")
-	for BC in problem_data["BCs"]["EBC"]:
-		node = BC["node"]
-		data = BC["data"][0]
-		F[node] = data
-		K[node, :] = 0.0
-		K[node, node] = 1.0
+    print("  * Applying EBCs...")
+    for BC in problem_data["BCs"]["EBC"]:
+        node = BC["node"]
+        data = BC["data"][0]
+        F[node] = data
+        K[node, :] = 0.0
+        K[node, node] = 1.0
 
-	print("  * Applying NBCs...")
-	NEN = problem_data["NEN"]
-	for BC in problem_data["BCs"]["NBC"]:
-		node1 = BC["face"]
-		node2 = (node1 + 1) % NEN
-		SV = BC["data"][0]
+    print("  * Applying NBCs...")
+    NEN = problem_data["NEN"]
+    for BC in problem_data["BCs"]["NBC"]:
+        node1 = BC["face"]
+        node2 = (node1 + 1) % NEN
+        SV = BC["data"][0]
 
-		e_nodes = problem_data["LtoG"][BC["element"]]
-		e_coord = get_element_coords(problem_data, e_nodes)
-		length = sqrt(((e_coord[node1] - e_coord[node2]) ** 2).sum())
+        e_nodes = problem_data["LtoG"][BC["element"]]
+        e_coord = get_element_coords(problem_data, e_nodes)
+        length = sqrt(((e_coord[node1] - e_coord[node2]) ** 2).sum())
 
-		SV *= .5 * length
-		F[e_nodes[node1]] += SV
-		F[e_nodes[node2]] += SV
+        SV *= .5 * length
+        F[e_nodes[node1]] += SV
+        F[e_nodes[node2]] += SV
 
-	print("  * Applying MBCs...")
-	for BC in problem_data["BCs"]["MBC"]:
-		element = BC["element"]
-		node1 = BC["face"]
-		node2 = (node1 + 1) % NEN
-		alpha = BC["data"][0]
-		beta = BC["data"][1]
+    print("  * Applying MBCs...")
+    for BC in problem_data["BCs"]["MBC"]:
+        element = BC["element"]
+        node1 = BC["face"]
+        node2 = (node1 + 1) % NEN
+        alpha = BC["data"][0]
+        beta = BC["data"][1]
 
-		e_nodes = problem_data["LtoG"][element]
-		e_coord = get_element_coords(problem_data, e_nodes)
-		length = sqrt(((e_coord[node1] - e_coord[node2]) ** 2).sum())
+        e_nodes = problem_data["LtoG"][element]
+        e_coord = get_element_coords(problem_data, e_nodes)
+        length = sqrt(((e_coord[node1] - e_coord[node2]) ** 2).sum())
 
-		node1 = e_nodes[node1]
-		node2 = e_nodes[node2]
-		F[node1] += 0.5 * beta * length
-		F[node2] += 0.5 * beta * length
-		K[node1, node1] -= (alpha * length) / 3.
-		K[node2, node2] -= (alpha * length) / 6.
+        node1 = e_nodes[node1]
+        node2 = e_nodes[node2]
+        F[node1] += 0.5 * beta * length
+        F[node2] += 0.5 * beta * length
+        K[node1, node1] -= (alpha * length) / 3.
+        K[node2, node2] -= (alpha * length) / 6.
 
-	return K, F
+    return K, F
--- src/solveproc.py
+++ src/solveproc.py
@@ -2,75 +2,75 @@
 @author: Deli, BYK
 @contact: gulen.ilker@hotmail.com, madbyk@gmail.com
 @summary:
-	Provides solving and post-processing functions.
-	Solving function uses SciPy's sparse solving function from sparse linear algebra module(sparse.spsolve).
-	Post process shapes the coordinates and results into a proper form to be plotted,
-	refines the plotting range and approximates mid values using node neighbors
-	and then produces a smooth contour plot.
+    Provides solving and post-processing functions.
+    Solving function uses SciPy's sparse solving function from sparse linear algebra module(sparse.spsolve).
+    Post process shapes the coordinates and results into a proper form to be plotted,
+    refines the plotting range and approximates mid values using node neighbors
+    and then produces a smooth contour plot.
 @version: 1.6
 '''
 
 def solve_system(K, F):
-	"""
-	Solves the K * x = F system using the linear sparse matrix solver.
-	The spsolve function used here can be replaced with qmr or some other
-	non-linear solver function for non-linear problems such as N-S problems.
-	"""
-	from scipy.sparse import linalg
+    """
+    Solves the K * x = F system using the linear sparse matrix solver.
+    The spsolve function used here can be replaced with qmr or some other
+    non-linear solver function for non-linear problems such as N-S problems.
+    """
+    from scipy.sparse import linalg
 
-	print("Solving system...")
-	return linalg.spsolve(K, F)
+    print("Solving system...")
+    return linalg.spsolve(K, F)
 
 def save_solution(file_name, solution):
-	from json import dump
+    from json import dump
 
-	print(" * Writing output file...")
-	output_file = open(file_name, "w")
-	dump({"T": solution.tolist()}, output_file)
-	output_file.close()
+    print(" * Writing output file...")
+    output_file = open(file_name, "w")
+    dump({"T": solution.tolist()}, output_file)
+    output_file.close()
 
 def plot_solution(problem_data, solution):
-	from numpy import linspace
-	from matplotlib.pylab import contour, colorbar, contourf, xlabel, ylabel, title, show
-	from matplotlib.mlab import griddata
+    from numpy import linspace
+    from matplotlib.pylab import contour, colorbar, contourf, xlabel, ylabel, title, show
+    from matplotlib.mlab import griddata
 
-	print(" * Preparing for plotting...")
-	NN = problem_data["NN"]
+    print(" * Preparing for plotting...")
+    NN = problem_data["NN"]
 
-	#Extract node coordinates seperately for plotting
-	x, y = [0] * NN, [0] * NN
-	for i, node in enumerate(problem_data["nodes"]):
-		x[i] = node[0]
-		y[i] = node[1]
+    #Extract node coordinates seperately for plotting
+    x, y = [0] * NN, [0] * NN
+    for i, node in enumerate(problem_data["nodes"]):
+        x[i] = node[0]
+        y[i] = node[1]
 
-	#refine the contour plot mesh for a "smoother" image, generate a 200*200 grid
-	xi = linspace(min(x), max(x), 200)
-	yi = linspace(min(y), max(y), 200)
-	#approximate the mid values from neighbors
-	zi = griddata(x, y, solution, xi, yi)
+    #refine the contour plot mesh for a "smoother" image, generate a 200*200 grid
+    xi = linspace(min(x), max(x), 200)
+    yi = linspace(min(y), max(y), 200)
+    #approximate the mid values from neighbors
+    zi = griddata(x, y, solution, xi, yi)
 
-	print(" * Plotting...")
-	#plot the contour lines with black
-	contour(xi, yi, zi, 15, linewidths = 0.5, colors = 'k')
-	#plot the filled contour plot
-	plot = contourf(xi, yi, zi, 15, antialiased = True)
+    print(" * Plotting...")
+    #plot the contour lines with black
+    contour(xi, yi, zi, 15, linewidths = 0.5, colors = 'k')
+    #plot the filled contour plot
+    plot = contourf(xi, yi, zi, 15, antialiased = True)
 
-	colorbar(plot, format = "%.3f").set_label("T")
-	xlabel('X')
-	ylabel('Y')
-	title("Contour plot of T values for {0}".format(problem_data["title"]))
+    colorbar(plot, format = "%.3f").set_label("T")
+    xlabel('X')
+    ylabel('Y')
+    title("Contour plot of T values for {0}".format(problem_data["title"]))
 
-	show()
+    show()
 
 def post_process(problem_data, solution, arguments):
-	"""
-	Performs the necessary post processing operations on the "solution"
-	using the problem_data such as contour plotting, SV calculating etc.
-	"""
-	print("Post processing...")
+    """
+    Performs the necessary post processing operations on the "solution"
+    using the problem_data such as contour plotting, SV calculating etc.
+    """
+    print("Post processing...")
 
-	if not arguments.dontsave:
-		save_solution(problem_data["output"], solution)
+    if not arguments.dontsave:
+        save_solution(problem_data["output"], solution)
 
-	if not arguments.dontplot:
-		plot_solution(problem_data, solution)
+    if not arguments.dontplot:
+        plot_solution(problem_data, solution)
